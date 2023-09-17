0cbbabbe6a37daade25d0b7393ae265dd0f00d6e
Demah <hamedsedighi@gmail.com>
2023-09-17 15:09:40 +0200

    add mood picker

---
+++ src/screens/Analytics.screen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const Analytics: React.FC = () => {
+  return <Text>Analytics</Text>;
+};
--- src/screens/ButtonTabs.navigator.tsx
+++ src/screens/ButtonTabs.navigator.tsx
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {Home} from './Home.screen';
 import {History} from './History.screen';
+import {Analytics} from './Analytics.screen';
 
 const BottomTabs = createBottomTabNavigator();
 
@@ -11,6 +12,7 @@ export const ButtomTabsNavigator: React.FC = () => {
     <BottomTabs.Navigator>
       <BottomTabs.Screen name="Home" component={Home} />
       <BottomTabs.Screen name="History" component={History} />
+      <BottomTabs.Screen name="Analytics" component={Analytics} />
     </BottomTabs.Navigator>
   );
 };
